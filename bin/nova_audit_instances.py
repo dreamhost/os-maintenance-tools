a42fa43b6f239eec33ddd2c9ae754f6e78aa1857
Jeremy Hanmer <jeremy@dreamhost.com>
2013-09-12 00:32:55 +0000

    nova auditor

---
+++ bin/nova_audit_instances.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import libvirt
+import os
+import sys
+from sqlalchemy import create_engine, select, MetaData, Table
+
+config = ConfigParser.ConfigParser()
+config.read(['os.cfg',os.path.expanduser('~/.os.cfg'),'/etc/os-maint/os.cfg'])
+
+os_user_name = config.get('OPENSTACK', 'os_user_name')
+os_password = config.get('OPENSTACK', 'os_password')
+os_tenant_name = config.get('OPENSTACK', 'os_tenant_name')
+os_auth_url = config.get('OPENSTACK', 'os_auth_url')
+os_region_name = config.get('OPENSTACK', 'os_region_name')
+connection_template = config.get('NOVA', 'libvirt_connection_template')
+
+from novaclient.v1_1 import client
+nc = client.Client(os_user_name, os_password, os_tenant_name, os_auth_url, service_type="compute")
+hosts = []
+
+nova_servers = dict()
+
+for s in nc.servers.list(True, {'all_tenants': '1'}):
+  name = s._info['OS-EXT-SRV-ATTR:instance_name']
+  hypervisor = s._info['OS-EXT-SRV-ATTR:hypervisor_hostname']
+  if not hypervisor in nova_servers:
+    nova_servers[hypervisor] = {} 
+  nova_servers[hypervisor][name] = s
+
+if len(sys.argv) > 1:
+  hosts = [sys.argv[1]]
+else:
+  for hv in nc.hypervisors.list():
+    hosts.append(hv.hypervisor_hostname)
+
+for hypervisor in hosts:
+  print 'Auditing {}'.format(hypervisor)
+  try:
+    conn = libvirt.open(connection_template.replace('$host', hypervisor))
+
+    for id in conn.listDomainsID():
+      dom = conn.lookupByID(id)
+      infos = dom.info()
+      if not dom.name() in nova_servers[hypervisor]:
+        print "{} not found in nova on {}".format(dom.name(), hypervisor)
+  except:
+    print "couldn't connect to {}".format(hypervisor)
+
