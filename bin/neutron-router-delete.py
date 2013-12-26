8225c2980b552ce332acffad3469912708e67940
Jeremy Hanmer <jeremy@dreamhost.com>
2013-12-26 22:46:23 +0000

    initial framework for router deletion

---
+++ bin/neutron-router-delete.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import libvirt
+import os
+import sys
+
+config = ConfigParser.ConfigParser()
+config.read(['os.cfg',os.path.expanduser('~/.os.cfg'),'/etc/os-maint/os.cfg'])
+
+os_user_name = config.get('OPENSTACK', 'os_user_name')
+os_password = config.get('OPENSTACK', 'os_password')
+os_tenant_name = config.get('OPENSTACK', 'os_tenant_name')
+os_auth_url = config.get('OPENSTACK', 'os_auth_url')
+os_region_name = config.get('OPENSTACK', 'os_region_name')
+
+from neutronclient.v2_0 import client
+nc = client.Client(username=os_user_name,
+                   password=os_password,
+                   tenant_name=os_tenant_name,
+                   auth_url=os_auth_url)
+
+if len(sys.argv) > 1:
+  router = sys.argv[1]
+  my_router = nc.show_router(router).get('router')
+#  print my_router.values()
+  print my_router.get('status')
+  if my_router.has_key('ports'):
+    print "We have ports"
+    ports = my_router.get('ports')
+    for port in ports:
+      print port.get('id')
