88a459f2b5ec8d3cc803a418b9caabd8529019d7
Jeremy Hanmer <jeremy@dreamhost.com>
2013-12-30 15:53:20 -0800

    mostly working, needs error checking

--- bin/neutron_router_delete.py
+++ bin/neutron_router_delete.py
@@ -22,16 +22,34 @@ nc = client.Client(username=os_user_name,
                    auth_url=os_auth_url)
 
 if len(sys.argv) > 1:
-    router = sys.argv[1]
-    my_router = nc.show_router(router).get('router')
+    router_id = sys.argv[1]
+    my_router = nc.show_router(router_id).get('router')
+    my_router_id = my_router.get('id')
     print "router status is {}".format(my_router.get('status'))
-    if 'ports' in my_router.keys():
+    ports = []
+    for port in nc.list_ports().get('ports'):
+        print port.keys()
+        device_id = port.get('device_id')
+        if device_id == router_id:
+            ports.append(port)
+    if ports:
         print "We have ports"
-        ports = my_router.get('ports')
         for port in ports:
+            if port.get('device_owner') == 'network:router_gateway':
+                print "skipping gateway port"
+                continue
             port_id = port.get('id')
             my_port = nc.show_port(port_id).get('port')
             print "port {} status {}".format(port_id,
                     my_port.get('status'))
+            info = {'id': my_router_id,
+                    'port_id': port_id,
+                    'tenant_id': my_router.get('tenant_id'),
+                    }
+            print port
+            nc.remove_interface_router(my_router_id, info)
     else:
         print "Router has no ports, continuing"
+    nc.delete_router(router_id)
+else:
+    print "Usage: neutron_router_delete.py [router_id]"
