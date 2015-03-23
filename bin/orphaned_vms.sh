2f11b4988d99794c6d3a480e399ab07b7a504a97
David Wahlstrom <david.wahlstrom@dreamhost.com>
2015-03-23 14:12:03 -0700

    Add orphaned_vms.sh script
    This is the initial commit for oprhaned_vms.sh.  This script will search
    through an openstack instance finding VMs who have a tenant_id that does
    not exist in keystone.

---
+++ bin/orphaned_vms.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# your OpenStack creds are expected to be in your environment variables
+
+vm_tenants=$(mktemp)
+keystone_tenants=$(mktemp)
+
+nova list --all-tenants --fields tenant_id | tail -n +4 | awk '{print $4}' | sort -u > $vm_tenants
+keystone tenant-list | tail -n +4 | awk '{print $2}' | sort -u > $keystone_tenants
+
+for tenant_id in `comm --nocheck-order -13 $keystone_tenants $vm_tenants`; do
+	nova list --all-tenants --tenant=$tenant_id --minimal | tail -n +4 | head -n -1
+done
+
+rm $keystone_tenants $vm_tenants
