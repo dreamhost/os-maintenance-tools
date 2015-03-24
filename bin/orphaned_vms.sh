a549436e3032584172f97ce7a1a15cdfb0e3676a
David Wahlstrom <david.wahlstrom@dreamhost.com>
2015-03-24 07:30:42 -0700

    Provide user with status of orphaned VM search
    Since this script can take a few moments to complete each step, this
    patch provides the user with some feedback, so they know where the
    process is at.

--- bin/orphaned_vms.sh
+++ bin/orphaned_vms.sh
@@ -5,9 +5,13 @@
 vm_tenants=$(mktemp)
 keystone_tenants=$(mktemp)
 
+echo -en "Retrieving list of all VMs...\r"
 nova list --all-tenants --fields tenant_id | tail -n +4 | awk '{print $4}' | sort -u > $vm_tenants
+
+echo -en "Retrieving list of all tenants...\r"
 keystone tenant-list | tail -n +4 | awk '{print $2}' | sort -u > $keystone_tenants
 
+echo -en "Comparing outputs to locate orphaned VMs....\r"
 for tenant_id in `comm --nocheck-order -13 $keystone_tenants $vm_tenants`; do
 	nova list --all-tenants --tenant=$tenant_id --minimal | tail -n +4 | head -n -1
 done
