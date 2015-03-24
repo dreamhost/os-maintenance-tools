94c15c67833ecb54123a1d2f371565bef69d2d10
David Wahlstrom <david.wahlstrom@dreamhost.com>
2015-03-24 07:51:07 -0700

    Provide some better headers and more fields
    To give us a little more context, this patch includes the headers
    (though not perfectly fitted) and some additional fields.

--- bin/orphaned_vms.sh
+++ bin/orphaned_vms.sh
@@ -12,8 +12,14 @@ echo -en "Retrieving list of all tenants...\r"
 keystone tenant-list | tail -n +4 | awk '{print $2}' | sort -u > $keystone_tenants
 
 echo -en "Comparing outputs to locate orphaned VMs....\r"
+iter=0
 for tenant_id in `comm --nocheck-order -13 $keystone_tenants $vm_tenants`; do
-	nova list --all-tenants --tenant=$tenant_id --minimal | tail -n +4 | head -n -1
+	if [[ $iter == 0 ]]; then
+		nova list --all-tenants --tenant=$tenant_id --fields tenant_id,name,status,created,updated | head -n -1
+		let "iter++"
+	else
+		nova list --all-tenants --tenant=$tenant_id --fields tenant_id,name,status,created,updated | tail -n +4 | head -n -1
+	fi
 done
 
 rm $keystone_tenants $vm_tenants
